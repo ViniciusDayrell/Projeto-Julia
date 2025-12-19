f74566fa425f223aba18bd72d01f250bafa93df3
Prepara Cursos <vinidayrell1@hotmil.com>
2025-12-19 18:57:33 -0300

    JS iniciado

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const form = document.getElementById("form")
+const nomeInput = document.getElementById("nome")
+const emailInput = document.getElementById("email")
+const comentarioInput = document.getElementById("mensagem")
+const listaComentarios = document.getElementById("lista-comentarios")
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const nome = nomeInput.value.trim()
+    const email = emailInput.value.trim()
+    const comentario = comentarioInput.value.trim()
+
+    if (!nome || !email || !comentario){
+        alert("Por favor, preencha todos os campos.")
+        nomeInput.focus()
+        return 
+    }
+})
